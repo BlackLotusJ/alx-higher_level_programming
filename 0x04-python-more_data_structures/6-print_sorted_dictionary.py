254e3a023405b99b73fc8a95546a0ce75950e776
BlackLotusJ <jerrykiche61@gmail.com>
2023-10-13 01:05:38 +0300

    corrected syntax error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -6,6 +6,6 @@ def print_sorted_dictionary(a_dictionary):
         return sorted(a_dictionary)
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
     print_sorted_dictionary(a_dictionary)
