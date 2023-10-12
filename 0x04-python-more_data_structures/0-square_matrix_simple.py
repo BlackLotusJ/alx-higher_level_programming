21dece84b277b6207e69293d5f25d4ed1e8e7018
BlackLotusJ <jerrykiche61@gmail.com>
2023-10-12 22:05:50 +0300

    created 0-square_matrix_simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix[]):
+    for row in range(len(matrix)):
+        for column in (len(matrix[0])):
+            square_matrix = (matrix[row][column]) * (matrix[row][column)
+    return square_matrix
