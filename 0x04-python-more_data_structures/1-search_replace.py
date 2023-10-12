13ffc21cc655f79d341fd27a4fae6c0fd1088fa9
BlackLotusJ <jerrykiche61@gmail.com>
2023-10-12 22:54:12 +0300

    fix systax error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for i in my_list:
-        if search = my_list[search]:
+        if search == my_list[search]:
             my_list = [replace]
 
