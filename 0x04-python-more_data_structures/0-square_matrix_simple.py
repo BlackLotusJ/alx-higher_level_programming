1fc733f75d7a2ca5a9e03d78517bf845a611a650
BlackLotusJ <jerrykiche61@gmail.com>
2023-10-12 22:23:42 +0300

    replaced with map and lamda function

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for row in range(len(matrix)):
-        for column in (len(matrix[0])):
-            square_matrix = (matrix[row][column]) * (matrix[row][column])
-    return square_matrix
+    return [list(map(lambda x: x ** 2, row))) for row in matrix]
