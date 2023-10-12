0aa4a4b31a94d5847fa7e39534ec0233cd492390
BlackLotusJ <jerrykiche61@gmail.com>
2023-10-13 01:04:19 +0300

    added 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is not None:
+        return sorted(a_dictionary)
+
+
+if __name__ = '__main__':
+    a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+    print_sorted_dictionary(a_dictionary)
