2808ef6c2ab5c993623047ead724878842e35bfc
BlackLotusJ <jerrykiche61@gmail.com>
2023-10-14 01:20:50 +0300

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,19 +3,19 @@
 
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        max_val = max(a_dictionary, key=a_dictionary.get())
-    return max_val
+        score = None
+        person = None
+        for k in a_dictionary.keys():
+            if score is None or a_dictionary[k] > score:
+                score = a_dictionary[k]
+                person = k
+        return person
 
 
 if __name__ == '__main__':
     best_score = __import__('10-best_score').best_score
 
-    a_dictionary = {'John': 12,
-            'Bob': 14,
-            'Mike': 14,
-            'Molly': 16,
-            'Adam': 10}
-
+    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
     best_key = best_score(a_dictionary)
     print("Best score: {}".format(best_key))
 
