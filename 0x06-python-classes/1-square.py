6e28e7ef79ec2b2183e0db1af4fda00444ad5965
BlackLotusJ <jerrykiche61@gmail.com>
2023-10-25 01:28:27 +0300

    correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,8 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """Square with size"""
 
 
 class Square:
-    """class Square tha tdefines a square"""
+    """class Square that defines a square"""
     def __init__(self, size):
         self.__size = size
