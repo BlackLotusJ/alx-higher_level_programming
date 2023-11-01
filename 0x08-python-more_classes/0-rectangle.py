ad6c9f13e3db169424abd21591e3b189f4d8f2a6
BlackLotusJ <jerrykiche61@gmail.com>
2023-11-01 21:10:31 +0300

    updated

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Rectangle Class.
+
+This module contains an empty class that defines a rectangle.
+
+"""
+
+
+class Rectangle:
+    """Defines the blueprint of a rectangle"""
+
+    pass
