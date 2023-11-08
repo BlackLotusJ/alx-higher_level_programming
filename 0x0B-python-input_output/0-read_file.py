b9544a6b58cb49f0b87b8194f01504e8f168beed
BlackLotusJ <jerrykiche61@gmail.com>
2023-11-09 00:57:16 +0300

    correction for 0-read_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""0-read_file.
+"""Module 0-read_file.
 Reads from a file and prints.
 """
 
+
 def read_file(filename=""):
     """Reads from filename and prints
-    its content to stdout
+    its contents to stdout.
 
     Args:
-        -filename: name of the file
+        - filename: name of the file
     """
+
     with open(filename) as f:
         read_text = f.read()
         print(read_text, end="")
