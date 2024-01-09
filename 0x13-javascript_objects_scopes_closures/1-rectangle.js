3f6bd543c762740f39c64262b906da936db03eca
BlackLotusJ <jerrykiche61@gmail.com>
2024-01-09 15:58:59 +0300

    correction -2

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor(height, width) {
-		this.height = height;
-		this.width = width;
+	constructor(w, h) {
+		this.h = height;
+		this.w = width;
 	}
 
 }
