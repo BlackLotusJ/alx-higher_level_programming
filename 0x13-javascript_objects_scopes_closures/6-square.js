08f9755ddeccd20653273a4ff6e45360cb1e6f2d
BlackLotusJ <jerrykiche61@gmail.com>
2024-01-11 04:58:07 +0300

    updated

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
-const SquareX = require('./5-square');
 
-class Square extends SquareX {
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
-    }
+const SquareModel = require('./5-square');
+
+module.exports = class Square extends SquareModel {
+  constructor (size) {
+    super(size, size);
   }
-}
 
-module.exports = Square;
+  charPrint (c) {
+    this.print(c);
+  }
+};
