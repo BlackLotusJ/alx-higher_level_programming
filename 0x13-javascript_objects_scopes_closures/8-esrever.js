9d5655ff11afdacd2c98695946be3f1186435d18
BlackLotusJ <jerrykiche61@gmail.com>
2024-01-11 04:56:03 +0300

    updated

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-exports.esrever = function (list) {
-  let start = 0;
-  let end = list.length - 1;
 
-  while (start < end) {
-    const temp = list[start];
-    list[start] = list[end];
-    list[end] = temp;
-    start++;
-    end++;
+exports.esrever = function (list) {
+  const reversed = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed.push(list[i]);
   }
+
+  return reversed;
 };
